9f3bd0d8f8bda6044461c4b0a3ea6cef147232df
Star1111 <380506803080@yandex.ua>
2020-10-23 14:24:32 +0300

    add enqueueSuspend fun

--- app/src/main/java/com/opninterviewservice/testapp/restapi/CorutineExt.kt
+++ app/src/main/java/com/opninterviewservice/testapp/restapi/CorutineExt.kt
@@ -1,6 +1,10 @@
 package com.opninterviewservice.testapp.restapi
 
 import com.opninterviewservice.testapp.interfaces.rest.base.AsyncApiCaller
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -34,6 +38,33 @@ suspend fun AsyncApiCaller.getPeople(): List<ShortPersonData> {
             continuation.resume(peoples)
         }, { e ->
             continuation.resumeWithException(e)
+        })
+    }
+}
+
+/**
+ * universal extension functions for executing async retrofit requests from coroutines
+ */
+
+suspend fun <T> Call<T>.enqueueSuspend(): T {
+    return suspendCoroutine { continuation ->
+        this.enqueue(object : Callback<T> {
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                if (response.isSuccessful) {
+                    response.body()?.apply { continuation.resume(this) }
+                        ?: continuation.resumeWithException(Exception("Empty body"))
+                } else {
+                    continuation.resumeWithException(
+                        Exception(
+                            response.code().toString() + (response.errorBody() ?: "")
+                        )
+                    )
+                }
+            }
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                continuation.resumeWithException(t)
+            }
         })
     }
 }
