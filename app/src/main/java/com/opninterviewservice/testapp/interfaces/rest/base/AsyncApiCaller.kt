01b29eeb0f4b6ea1c33661162c63ed615cc820c2
Star1111 <380506803080@yandex.ua>
2020-10-23 15:01:48 +0300

    some dok Add

--- app/src/main/java/com/opninterviewservice/testapp/interfaces/rest/base/AsyncApiCaller.kt
+++ app/src/main/java/com/opninterviewservice/testapp/interfaces/rest/base/AsyncApiCaller.kt
@@ -7,7 +7,19 @@ import com.opninterviewservice.testapp.restapi.ShortPersonData
 //This File Created at 23.10.2020 10:49.
 interface AsyncApiCaller {
 
+    /**
+     * returns a list of people the implementation must be asynchronous
+     * @param onSusses callback called in case of success
+     * @param onError callback called in case of error
+     */
     fun getPeople(onSusses: (List<ShortPersonData>) -> Unit, onError: (Throwable) -> Unit)
 
+
+    /**
+     * returns a data of person the implementation must be asynchronous
+     * @param id ID of the person from the list of people
+     * @param onSusses callback called in case of success
+     * @param onError callback called in case of error
+     */
     fun getPersonData(id: String, onSusses: (PersonData) -> Unit, onError: (Throwable) -> Unit)
 }
--- app/src/main/java/com/opninterviewservice/testapp/interfaces/rest/base/BlockingApiCaller.kt
+++ app/src/main/java/com/opninterviewservice/testapp/interfaces/rest/base/BlockingApiCaller.kt
@@ -7,7 +7,16 @@ import com.opninterviewservice.testapp.restapi.ShortPersonData
 //This File Created at 23.10.2020 10:50.
 interface BlockingApiCaller {
 
+    /**
+     * returns a list of people the implementation must be synchronous
+     * @return list of people
+     */
     fun getPeople(): List<ShortPersonData>
 
+    /**
+     * returns a data of person the implementation must be synchronous
+     * @param id ID of the person from the list of people
+     * @return data of person
+     */
     fun getPersonData(id: String): PersonData
 }
