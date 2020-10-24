94cff697abbf5d21fb713143a952238f6f892db8
Star1111 <380506803080@yandex.ua>
2020-10-24 14:51:16 +0300

    PersonInfoFragment UI Add

--- app/src/main/java/com/opninterviewservice/testapp/ui/main/fragments/PersonInfoFragment.kt
+++ app/src/main/java/com/opninterviewservice/testapp/ui/main/fragments/PersonInfoFragment.kt
@@ -9,13 +9,16 @@ import android.widget.Toast
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import com.opninterviewservice.testapp.R
+import com.opninterviewservice.testapp.restapi.PersonData
 import com.opninterviewservice.testapp.ui.main.fragments.viewmodels.BaseViewModel
 import com.opninterviewservice.testapp.ui.main.fragments.viewmodels.PersonInfoFragmentViewModel
-import kotlinx.android.synthetic.main.people_list_fragment.*
+import kotlinx.android.synthetic.main.people_info_fragment.*
+import kotlinx.android.synthetic.main.people_list_fragment.emptyMessage
+import kotlinx.android.synthetic.main.people_list_fragment.spinner
 
 const val ID_KEY = "id"
-//                        TODO change it to real layout
-class PersonInfoFragment : Fragment(R.layout.people_list_fragment) {
+
+class PersonInfoFragment : Fragment(R.layout.people_info_fragment) {
 
     companion object {
         fun newInstance(args: Bundle): PersonInfoFragment {
@@ -29,11 +32,15 @@ class PersonInfoFragment : Fragment(R.layout.people_list_fragment) {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get((PersonInfoFragmentViewModel::class.java))
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get((PersonInfoFragmentViewModel::class.java))
         viewModel.getState().observe(this as LifecycleOwner, {
             when (it?.state) {
                 BaseViewModel.UIStates.LOADING -> {
                     if (it.data == true) {
+                        changeUIVisibility(VisibilityState.GONE)
                         spinner.visibility = View.VISIBLE
                         emptyMessage.visibility = View.GONE
                     } else {
@@ -45,11 +52,15 @@ class PersonInfoFragment : Fragment(R.layout.people_list_fragment) {
                     spinner.visibility = View.GONE
                     changeUIVisibility(VisibilityState.GONE)
                     if (it.data is Int) {
-                        Toast.makeText(context!!.applicationContext,
-                            it.data, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            context!!.applicationContext,
+                            it.data, Toast.LENGTH_SHORT
+                        ).show()
                     } else {
-                        Toast.makeText(context!!.applicationContext,
-                            it.data as String, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            context!!.applicationContext,
+                            it.data as String, Toast.LENGTH_SHORT
+                        ).show()
                         emptyMessage.visibility = View.VISIBLE
                         emptyMessage.text = it.data
                     }
@@ -59,9 +70,9 @@ class PersonInfoFragment : Fragment(R.layout.people_list_fragment) {
                     spinner.visibility = View.GONE
                     viewModel.currentPerson?.apply {
                         emptyMessage.visibility = View.GONE
-//                        TODO set UI values here
+                        setUIValues(this)
                         changeUIVisibility(VisibilityState.VISIBLE)
-                         } ?: run {
+                    } ?: run {
                         emptyMessage.visibility = View.VISIBLE
                         changeUIVisibility(VisibilityState.GONE)
                     }
@@ -70,14 +81,29 @@ class PersonInfoFragment : Fragment(R.layout.people_list_fragment) {
         })
     }
 
-    private fun changeUIVisibility(visibility: VisibilityState){
+    private fun changeUIVisibility(visibility: VisibilityState) {
 
-        idsItems.visibility = visibility.state
+        firstName.visibility = visibility.state
+        firstNameValue.visibility = visibility.state
+        lastName.visibility = visibility.state
+        lastNameValue.visibility = visibility.state
+        age.visibility = visibility.state
+        ageValue.visibility = visibility.state
+        country.visibility = visibility.state
+        countryValue.visibility = visibility.state
+    }
+
+    private fun setUIValues(data: PersonData) {
+
+        firstNameValue.text = data.firstName
+        lastNameValue.text = data.lastName
+        ageValue.text = data.age.toString()
+        countryValue.text = data.country
     }
 
     override fun onStart() {
         super.onStart()
-        viewModel.requestPersonInfo(arguments?.getString(ID_KEY,"") ?: "")
+        viewModel.requestPersonInfo(arguments?.getString(ID_KEY, "") ?: "")
     }
 
     enum class VisibilityState(val state: Int) {
